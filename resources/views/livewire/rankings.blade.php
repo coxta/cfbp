71e2063c36e19dca2fe126a335173707424bddf6
Taylor Cox <taylor@coxes.family>
2023-10-18 09:17:41 -0400

    Rankings responsiveness

--- resources/views/livewire/rankings.blade.php
+++ resources/views/livewire/rankings.blade.php
@@ -25,22 +25,22 @@
 
         <div class="flex items-center space-x-4 justify-between text-sm p-2 font-bold">
 
-            <!-- Maatchup Flex -->
-            <div class="flex w-2/6">
-                Team</a>
+            <!-- Matchup Flex -->
+            <div class="flex w-1/2 md:w-1/3">
+                Team
             </div>
 
-            <div class="flex w-1/6">
+            <div class="flex w-1/6 justify-center">
                 Record
             </div>
 
-            <div class="flex w-1/6">
+            <div class="flex w-1/6 justify-center">
                 Trend
             </div>
-            <div class="flex w-1/6">
+            <div class="hidden md:flex w-1/6 justify-center">
                 Points
             </div>
-            <div class="flex w-1/6">
+            <div class="hidden md:flex w-1/6 justify-center">
                 Votes
             </div>
         </div>
@@ -52,24 +52,32 @@
                 <div class="flex items-center px-4 space-x-4 justify-between text-sm p-1.5">
 
                     <!-- Maatchup Flex -->
-                    <div class="flex w-2/6 items-center">
+                    <div class="flex w-1/2 md:w-1/3 items-center">
                         <div class="font-semibold text-gray-500 mr-4">{{ $rank['rank'] }}</div>
                         <img class="h-5 w-5 mr-2" src="{{ $rank->team->logo }}">
                         <a href="{{ route('team', ['team' => $rank->team->id]) }}"
                             class="hover:text-blue-600 text-md">{{ $rank->team->location }}</a>
                     </div>
 
-                    <div class="flex w-1/6">
+                    <div class="flex w-1/6 justify-center">
                         {{ $rank['record'] }}
                     </div>
 
-                    <div class="flex w-1/6">
-                        {{ $rank['trend'] }}
+                    <div class="flex items-center space-x-1 w-1/6 justify-center font-semibold">
+                        @if(strlen($rank['trend']) > 1 && substr($rank['trend'],0,1) == '+')
+                            <x-bi-arrow-up-short class="w-4 h-4 text-green-600"/>
+                            <span class="text-green-600">{{ substr($rank['trend'],1) }}</span>
+                        @elseif(strlen($rank['trend']) > 1 && substr($rank['trend'],0,1) == '-')
+                            <x-bi-arrow-down-short class="w-4 h-4 text-red-600"/>
+                            <span class="text-red-600">{{ substr($rank['trend'],1) }}</span>
+                        @else
+                            <span class="text-gray-600">{{ $rank['trend'] }}</span>
+                        @endif
                     </div>
-                    <div class="flex w-1/6">
+                    <div class="hidden md:flex w-1/6 justify-center">
                         {{ $rank['points'] }}
                     </div>
-                    <div class="flex w-1/6">
+                    <div class="hidden md:flex w-1/6 justify-center">
                         {{ $rank['votes'] }}
                     </div>
                 </div>
