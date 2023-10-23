b3605e5cbd0765ae9558141785109316b0575e95
Taylor Cox <taylor@coxes.family>
2023-10-23 16:08:56 -0400

    Teams route basic

--- app/Livewire/Teams.php
+++ app/Livewire/Teams.php
@@ -3,11 +3,110 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use App\Models\Team;
+use App\Models\Conference;
 
 class Teams extends Component
 {
+
+    #[Url(as: 'div', keep: true, history: true)]
+    public $division = 'FBS';
+
+    #[Url(as: 'conf', keep: true, history: true)]
+    public $conference = 0;
+
+    public $divisions = [];
+    public $conferences = [];
+
+    public $conferenceIds = [];
+
+    public $teams;
+    
     public function render()
     {
+        $this->filters();
+        $this->teams();
         return view('livewire.teams');
     }
+
+    public function teams()
+    {
+        if($this->conference > 0) {
+            $this->teams = Team::without('conference')
+                            ->select([
+                                'teams.id as id',
+                                'teams.name as name',
+                                'teams.location as location',
+                                'teams.logo as logo',
+                                'conferences.id as conference_id',
+                                'conferences.abbr as conference_abbr'
+                            ])
+                            ->join('conferences', 'conferences.id', '=', 'teams.conference_id')
+                            ->where('conference_id', $this->conference)
+                            ->orderBy('conferences.abbr')
+                            ->orderBy('teams.location')
+                            ->get();
+        } else {
+            $this->teams = Team::without('conference')
+                            ->select([
+                                'teams.id as id',
+                                'teams.name as name',
+                                'teams.location as location',
+                                'teams.logo as logo',
+                                'conferences.id as conference_id',
+                                'conferences.abbr as conference_abbr'
+                            ])
+                            ->join('conferences', 'conferences.id', '=', 'teams.conference_id')
+                            ->whereIn('conference_id', $this->conferenceIds)
+                            ->orderBy('conferences.abbr')
+                            ->orderBy('teams.location')
+                            ->get();
+        }
+    }
+
+    public function filters()
+    {
+        
+        $this-> divisions = [];
+
+        array_push($this->divisions, [
+            'name' => 'FBS',
+            'value' => 'FBS'
+        ]);
+        array_push($this->divisions, [
+            'name' => 'FCS',
+            'value' => 'FCS'
+        ]);
+        array_push($this->divisions, [
+            'name' => 'Div II',
+            'value' => 'Div II'
+        ]);
+        array_push($this->divisions, [
+            'name' => 'Div III',
+            'value' => 'Div III'
+        ]);
+
+        $this->conferences = [];
+        $this->conferenceIds = [];
+
+        $conferences = Conference::where('division', $this->division)->orderBy('abbr')->get();
+
+        array_push($this->conferences, [
+            'name' => 'All',
+            'value' => 0
+        ]);
+
+        foreach ($conferences as $c) {
+
+            array_push($this->conferences, [
+                'name' => $c->abbr,
+                'value' => $c->id
+            ]);
+
+            array_push($this->conferenceIds, $c->id);
+
+        }
+
+    }
+
 }
--- resources/views/livewire/teams.blade.php
+++ resources/views/livewire/teams.blade.php
@@ -1,9 +1,43 @@
 <div>
+
     @slot('title')
         Teams
     @endslot
 
-    <div class="text-xl font-semibold text-gray-600">
-        Teams
+    <div class="flex flex-col space-y-1 md:space-y-0 md:flex-row md:items-center md:space-x-4 md:justify-between">
+        <div class="flex text-md font-semibold text-gray-700">
+            Teams
+        </div>
+        <div class="flex flex-row items-center space-x-4">
+            <x-input.select wire:model.live="division" :options="$divisions" />
+            <x-input.select wire:model.live="conference" :options="$conferences" />
+        </div>
     </div>
+
+    <div class="bg-white rounded-md shadow-md my-4 p-4">
+
+        @php 
+            $conf = 0;
+        @endphp
+
+        @foreach($teams as $team)
+
+            @if($team->conference_id != $conf)
+                @php 
+                    $conf = $team->conference_id;
+                @endphp
+                <div class="text-gray-700 font-bold text-sm py-2.5 mb-2 border-b border-dotted border-gray-700">
+                    {{ $team->conference_abbr }}
+                </div>
+            @endif
+                <a href="/teams/{{ $team->id }}" class="flex flex-row ml-2 items-center space-x-4 py-1 cursor-pointer text-gray-800 hover:text-blue-800 {{ $loop->last ? '' : 'border-b border-gray-200' }}">
+                    <img src="{{ $team->logo }}" class="w-8 h-8" />
+                    <span class="font-bold">
+                        {{ $team->location . ' ' . $team->name }}
+                    </span>
+                </a>
+        @endforeach
+
+    </div>
+
 </div>
