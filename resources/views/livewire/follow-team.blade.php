8f93b9f1e19b9be8662c41c60f5e79ea601cb477
Taylor Cox <taylor@coxes.family>
2024-08-24 17:23:10 -0400

    Follow team bug when not authenticated

--- resources/views/livewire/follow-team.blade.php
+++ resources/views/livewire/follow-team.blade.php
@@ -1,7 +1,11 @@
 <span>
-    @if($following)
-        <x-button wire:click="toggle" spinner="toggle" label="Unfollow" secondary outline/>
+    @auth()
+        @if ($following)
+            <x-button wire:click="toggle" spinner="toggle" label="Unfollow" secondary outline />
+        @else
+            <x-button wire:click="toggle" spinner="toggle" label="Follow" icon="plus" primary />
+        @endif
     @else
-        <x-button wire:click="toggle" spinner="toggle" label="Follow" icon="plus" primary/>
-    @endif
+        <x-button href="{{ route('login') }}" label="Follow" icon="plus" primary />
+    @endauth
 </span>
